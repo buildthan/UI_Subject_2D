54df37b743e2dd9d2a901e23c3753d363ea5207f
buildthan <a9175226@gmail.com>
2025-03-25 14:40:35 +0900

    [Feat] 인벤토리 UI 기본 틀 구성

--- Assets/Scripts/UIInventory.cs
+++ Assets/Scripts/UIInventory.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIInventory : BaseUI
 {
+    public Button mainMenuButton;
+
     protected override UIState GetUIState()
     {
         return UIState.Inventory;
